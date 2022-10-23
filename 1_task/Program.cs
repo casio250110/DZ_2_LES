16e2a2d1323e67adefeebbfbfa7b73570d2f2984
casio250110 <casio250110@yandex.ru>
2022-10-23 23:50:14 +0300

    Решили первую задачу

---
+++ 1_task/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = new Random().Next(100, 999);
+Console.WriteLine(number);
+int result = (number % 100) / 10;
+Console.WriteLine($"Вторая цифра числа {number} = {result}");
